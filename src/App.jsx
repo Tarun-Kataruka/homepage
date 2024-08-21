2cd323379d47522daeaeb51c7bb4f99585681795
Tarun Kataruka <tarunkataruka@Taruns-MacBook-Air.local>
2024-08-22 01:09:49 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import LandingPage from  "./LandingPage";
 
-import './App.css'
-import Navbar from './Components/Navbar'
-import LandingPage from './landingPage'
 
 function App() {
-
   return (
     <>
-    <Navbar className="w-full" />
-    <LandingPage />
+      <Navbar className="w-full" />
+      <LandingPage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
